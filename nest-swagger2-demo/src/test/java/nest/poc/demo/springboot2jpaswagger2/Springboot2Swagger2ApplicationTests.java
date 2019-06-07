41f9e7abadb24175039268904e8c2e699174de0a
cgkbharadwaj <cgkbharadwaj@gmail.com>
2019-06-07 23:35:55 +0100

    First Commit

---
+++ nest-swagger2-demo/src/test/java/nest/poc/demo/springboot2jpaswagger2/Springboot2Swagger2ApplicationTests.java
@@ -0,0 +1,16 @@
+package nest.poc.demo.springboot2jpaswagger2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={Springboot2Swagger2ApplicationTests.class})
+public class Springboot2Swagger2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
